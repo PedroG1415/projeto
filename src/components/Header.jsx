755baaa6e60c5c3767dbf8bc7fcb60b31e86e2ff
Pedro <pedro.00000848852@unicap.br>
2025-09-17 12:42:19 -0300

    Header att

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import MenuItem from './MenuItem'
 import {AiFillHome} from 'react-icons/ai'
 import {BsFillInfoCircleFill} from 'react-icons/bs'
 import Link from 'next/link'
+import { FaStar } from "react-icons/fa";
 
 export default function Header() {
   return (
@@ -10,11 +11,13 @@ export default function Header() {
         <div className='flex gap-4'>
         <MenuItem title="Home" address="/" Icon={AiFillHome}/>
         <MenuItem title="Sobre" address="/about" Icon={BsFillInfoCircleFill}/>
+        <MenuItem title='Favoritos' address="/favoritos" Icon={FaStar}/>
         </div>
         <Link href={'/'} className='flex gap-1 items-center'>
             <span className='text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg'>IMDb</span>
             <span className='text-xl hidden sm:inline'>Filmes</span>
         </Link>
+
     </div>
   )
 }
