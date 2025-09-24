b362396a47ce8a41cd84fb2c7f97e5e913d324c2
Pedro <pedro.00000848852@unicap.br>
2025-09-24 20:06:22 -0300

    Sobre e DarkMode

---
+++ src/app/about/page.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4'>
+      <h1 className='text-2xl font-medium text-amber-600'>Sobre </h1>
+      <p>
+  Bem-vindo ao nosso site de filmes! Somos uma equipe de apaixonados por cinema que se uniu para criar um espaço completo para tudo relacionado a filmes.
+</p>
+
+<p>
+  Nosso site oferece um banco de dados abrangente de filmes de todas as partes do mundo, além das últimas notícias, críticas e trailers. Atualizamos constantemente nossa coleção com novos lançamentos, garantindo que você tenha acesso sempre às novidades do cinema. Você pode buscar filmes por título, diretor, ator, gênero ou ano de lançamento, facilitando encontrar o filme perfeito para qualquer ocasião.
+</p>
+
+<p>
+  Além do banco de dados, nossa plataforma também conecta fãs de cinema, permitindo que você compartilhe suas opiniões sobre os lançamentos mais recentes. Temos um fórum para discutir filmes com pessoas que têm os mesmos interesses, além de avaliações e críticas de outros usuários. Também oferecemos uma seção dedicada a notícias e trailers, mantendo você sempre atualizado.  
+  Agradecemos a sua visita e esperamos que aproveite sua experiência navegando pelo nosso site. Se tiver sugestões ou feedback, não hesite em nos contatar — estamos sempre buscando melhorar a experiência para nossos usuários. Boa diversão!
+</p>
+
+    </div>
+  )
+}
